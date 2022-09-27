df90b17546737a577ff35a32c476deba103aac60
Elliott <elliottwink@hotmail.com>
2022-09-27 17:50:22 -0500

    updated elabel functionality and timer

--- wwwroot/js/minesweeper_board.js
+++ wwwroot/js/minesweeper_board.js
@@ -6,7 +6,7 @@
     let bombs = [];
     let valid = [];
     let numValidChecked = 0;
-    let _board = [];
+    let sqSize = 40;
 
     function click(board, square) {
         if (square.classList.contains("flag")) { return; }
@@ -27,14 +27,11 @@
             let total = square.getAttribute("data");
             square.classList.add("checked");
             if (numValidChecked === valid.length) { gameOver(); }
+            updateNeighbors(board, square);
             if (total != 0 && total != 9) {
                 square.innerHTML = total;
                 square.classList.add("checked" + total);
                 setEffectiveLabel(board, square);
-                if (document.getElementById("effectiveLabelCheck").checked) {
-                    square.classList.add("effective-label");
-                    square.innerHTML = square.getAttribute("elabel");
-                }
                 return;
             }
             checkSquare(board, square, currentId);
@@ -56,6 +53,7 @@
                 let c = n[1];
                 if (r >= 0 && c >= 0 && c < board.length && r < (board[0]).length) {
                     var curr = document.getElementById(r + "," + c);
+                    curr.setAttribute("markedNeighbors", parseInt(curr.getAttribute("markedNeighbors")) - 1);
                     setEffectiveLabel(board, curr);
                 }
             }
@@ -63,6 +61,7 @@
         }
         square.classList.add("flag");
         square.innerHTML = "&#128681";
+        updateNeighbors(board, square);
         let coords = square.getAttribute("id").split(",");
         let i = parseInt(coords[0]);
         let j = parseInt(coords[1]);
@@ -103,6 +102,7 @@
         var j = parseInt(coords[1]);
         var neighbors = [[i - 1, j + 1], [i, j + 1], [i + 1, j + 1], [i + 1, j], [i + 1, j - 1], [i, j - 1], [i - 1, j - 1], [i - 1, j]];
         let elabel = parseInt(square.getAttribute("data"));
+        let coveredNeighbors = [];
         for (let z = 0; z < neighbors.length; ++z) {
             let n = neighbors[z];
             let r = n[0];
@@ -110,13 +110,59 @@
             if (r >= 0 && c >= 0 && c < board.length && r < (board[0]).length) {
                 let curr = document.getElementById(r + "," + c);
                 if (curr.classList.contains("flag")) {
-                    --elabel;  
+                    --elabel;
+                }
+                else if (!curr.classList.contains("checked")) {
+                    coveredNeighbors.push(curr);
                 }
             }
         }
         square.setAttribute("elabel", elabel);
-        if (square.classList.contains("effective-label")) {
+        if (elabel === 0 && coveredNeighbors.length === 0) { square.setAttribute("elabelComplete", "1"); }
+        else { square.setAttribute("elabelComplete", "0"); }
+        if (document.getElementById("effectiveLabelCheck").checked && square.classList.contains("checked")) {
+            if (!square.classList.contains("effective-label") && !square.classList.contains("effective-label0") && !square.classList.contains("effective-label-negative")) { !square.classList.add("effective-label") }
             square.innerHTML = elabel;
+            if (elabel === 0) {
+                if (coveredNeighbors.length === 0) {
+                    square.classList.replace("effective-label", "effective-label0");
+                    square.classList.replace("effective-label-negative", "effective-label0");
+                }
+                else {
+                    square.classList.replace("effective-label-negative", "effective-label");
+                    square.setAttribute("elabelComplete", "0");
+                }
+            }
+            else if (elabel < 0) {
+                square.classList.replace("effective-label0", "effective-label-negative");
+                square.classList.replace("effective-label", "effective-label-negative");
+                square.setAttribute("elabelComplete", "0");
+            }
+            else {
+                square.classList.replace("effective-label0", "effective-label");
+                square.classList.replace("effective-label-negative", "effective-label");
+                square.setAttribute("elabelComplete", "0");
+            }
+        }
+    }
+
+    function updateNeighbors(board, square) {
+        var coords = square.getAttribute("id").split(",");
+        var i = parseInt(coords[0]);
+        var j = parseInt(coords[1]);
+        var neighbors = [[i - 1, j + 1], [i, j + 1], [i + 1, j + 1], [i + 1, j], [i + 1, j - 1], [i, j - 1], [i - 1, j - 1], [i - 1, j]];
+        for (let z = 0; z < neighbors.length; ++z) {
+            let n = neighbors[z];
+            let r = n[0];
+            let c = n[1];
+            if (r >= 0 && c >= 0 && c < board.length && r < (board[0]).length) {
+                let curr = document.getElementById(r + "," + c);
+                let markedNeighbors = parseInt(curr.getAttribute("markedNeighbors")) + 1;
+                curr.setAttribute("markedNeighbors", markedNeighbors);
+                if (markedNeighbors === 8 && curr.getAttribute("elabel") === "0" && curr.classList.contains("effective-label")) {
+                    curr.classList.replace("effective-label", "effective-label0");
+                }
+            }
         }
     }
 
@@ -124,6 +170,9 @@
         var t = Math.floor((Date.now() - startTime) / 1000);
         if (!isGameOver) {
             document.getElementById("timeElapsed").innerHTML = t + " seconds";
+            setTimeout(() => {
+                elapsedTime();
+            }, 1000);
         }
         return t;
     }
@@ -132,21 +181,25 @@
         const height = board.length;
         const width = board[0].length;
         const grid = document.querySelector('.grid');
-        document.getElementById("msBoard").style.height = (40 * height) + "px";
-        document.getElementById("msBoard").style.width = (40 * width) + "px";
+        document.getElementById("msBoard").style.height = (sqSize * height) + "px";
+        document.getElementById("msBoard").style.width = (sqSize * width) + "px";
         for (var i = 0; i < height; ++i) {
             for (var j = 0; j < width; ++j) {
                 const square = document.createElement('div');
                 square.setAttribute('id', j + "," + i);
-                square.setAttribute('elabel', 0);
+                square.setAttribute('elabel', "0");
+                square.setAttribute("elabelComplete", "0");
+                square.setAttribute("markedNeighbors", "0");
+                square.style.width = sqSize + "px";
+                square.style.height = sqSize + "px";
                 if (board[i][j] === 9) { square.classList.add("bomb"); }
                 else { square.classList.add("valid"); }
                 grid.appendChild(square);
                 square.addEventListener('click', function (e) {
                     click(board, square);
                 })
-                square.addEventListener('contextmenu', function (ev) {
-                    ev.preventDefault();
+                square.addEventListener('contextmenu', function (e) {
+                    e.preventDefault();
                     rightClick(board, square);
                 }, false);
             }
@@ -161,11 +214,13 @@
             boardWidth = 16;
             boardHeight = 16;
             numOfMines = 40;
+            sqSize = 30;
         }
         if (difficulty === "Expert") {
             boardWidth = 30;
             boardHeight = 16;
             numOfMines = 99;
+            sqSize = 30;
         }
 
         let board = new Array(boardHeight);
